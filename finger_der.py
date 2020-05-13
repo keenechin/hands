d464d9b01d85a9c2bb13650c4f2e9c122a943a38
Keene Chin <keene.chin@gmail.com>
2020-05-13 12:36:26 -0400

    optimization of waypoint coincidence working for endpoint

--- finger_der.py
+++ finger_der.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.lines as mline
 from functools import partial
-
+import skopt
+from scipy.spatial.distance import cdist
 #%%
 def transform2d(rot_ang,trans_vec):
     o = rot_ang
@@ -78,7 +79,7 @@ def update_frame(points,ax,line_artists):
 def transform_points(points0,theta1=0,theta2=0):
     end_tf = get_matrices([[0,0],[theta1,0],[theta1,0],[theta2,0],[theta2,0]]) 
 
-    updated_points = points0
+    updated_points = np.copy(points0)
 
     for i,mat in enumerate(end_tf):
         frame_origin = updated_points[i,:]
@@ -88,59 +89,122 @@ def transform_points(points0,theta1=0,theta2=0):
             updated_points[i+j,:] = u_pt+frame_origin
     return updated_points
 
-def theta_frames(points0,u_start = 0, u_end = 4, compl1=2, compl2=4, n_frames = 30):
+def generate_point_frames(points0, compl, max_angle, u_start, u_end, n_frames):
     assert(u_end>u_start)
     o1,o2 = 0,0
+    max_kappa = 2*np.arctan2(max_angle[0],2), 2*np.arctan2(max_angle[1],2)
     for u in np.linspace(u_start,u_end,n_frames):
-        kappa1 = compl1*u/500
-        kappa2 = compl2*u/500
+        curves1 = [compl[0]*u/50., max_kappa[0]/2.]
+        curves2 = [compl[1]*u/50., max_kappa[1]/2.]
+        kappa1 = min(curves1)
+        kappa2 = min(curves2) 
         o1 = 2*np.tan(kappa1/2)
         o2 = 2*np.tan(kappa2/2)
-        points = transform_points(points0,theta1=o1,theta2=o2)
+        #print(np.degrees(o2))
+        points = transform_points(points0,theta1=o1, theta2=o2)
+        #print(points)
         yield points
 
+def initialize_points(total_length, joint_length,l0):
+    l1 = (total_length - (2*joint_length + l0))/2
+    l2 = l1
+    lengths = [l0,joint_length,l1,joint_length,l2]
+    n_rods = len(lengths)
+    n_points = n_rods+1
+    tf_params0 = np.zeros((n_rods,2))
+    tf_params0[:,1] = lengths
+    mats = get_matrices(tf_params0)
 
+    #get initial config of nodes
+    points0 = pt_h([(0,0)])
+    pt = points0[0]
+    for mat in mats:
+        pt = mat@pt
+        points0 = np.append(points0,np.array([pt]),axis=0)
+    return points0
+
+def simulate_trajectory(points0, compl1, compl2 , max_ang1, max_ang2, u_start = 0, u_end = 10, n_frames = 100):
+    points_generator = generate_point_frames(points0, compl = (compl1, compl2),  max_angle = (max_ang1, max_ang2), u_start = u_start, u_end = u_end, n_frames = n_frames)
+    tip_trajectory = []
+    for points in points_generator:
+        tip_trajectory.append([points[-1][0],points[-1][1]])
+    tip_trajectory = np.array(tip_trajectory)
+    return np.squeeze(tip_trajectory, axis=2)
+
+
+def trajectory_cost(trajectory,waypoints):
+    endpoint_cost = np.linalg.norm([trajectory[-1,0]-waypoints[-1,0], trajectory[-1,1]-waypoints[-1,1]])
+    trajectory_distance_mat = cdist(trajectory,waypoints)
+    waypoint_min_dists = np.min(trajectory_distance_mat,axis = 0)
+    trajectory_cost = np.sum(waypoint_min_dists[:])
+    cost = 0.5*endpoint_cost + 0.5*trajectory_cost 
+    return cost
+
+def get_params_cost(x,total_length,joint_length, waypoints):
+    l0, compl1, compl2, max_ang1, max_ang2 = x
+    points = initialize_points(total_length,joint_length,l0)
+    trajectory = simulate_trajectory(points, compl1, compl2, max_ang1, max_ang2)
+    cost = trajectory_cost(trajectory, waypoints)
+    return cost
 
 #%%
-total_length = 90
-l0 = 15
-l1 = (total_length - l0)/2
-l2 = l1
-lengths = [l0,10,l1,10,l2]
-total_length = sum(lengths)
-n_rods = len(lengths)
-n_points = n_rods+1
-tf_params0 = np.zeros((n_rods,2))
-tf_params0[:,1] = lengths
-mats = get_matrices(tf_params0)
+visualize = False
 
-#get initial config of nodes
-points0 = pt_h([(0,0)])
-pt = points0[0]
-for mat in mats:
-    pt = mat@pt
-    points0 = np.append(points0,np.array([pt]),axis=0)
-
-
-
-fig= plt.figure(frameon=False)
-viewport = fig.add_axes([0,0,1,1])
-viewport.xaxis.set_visible(False)
-viewport.yaxis.set_visible(False)
-bound = total_length+10
-viewport.axis('equal')
-viewport.set_xlim(-bound,bound)
-viewport.set_ylim(-bound,bound)
-draw_lines0 = partial(draw_lines,viewport,points0)
-artists = draw_lines0()
-# %%
-fps = 15
-writer = animation.writers['ffmpeg']
-writer = writer(fps = fps, metadata=dict(artist='Keene Chin'), bitrate=1000)
+total_length, joint_length, l0 = 100, 10 , 30 
 compl1 = 2
-compl2 = 2
-ani = animation.FuncAnimation(fig,func=update_frame,interval=1000//fps,frames=theta_frames(points0,compl1=compl1,compl2=compl2),blit=True,fargs=[viewport,artists])
-ani.save('./finger_l{}_c{}_c{}.mp4'.format(l0,compl1,compl2), writer=writer)
+compl2 = 4
+max_ang1 = np.pi
+max_ang2 = np.pi
+
+
+waypoints = np.array([[40,20],[0,20]])
+
+x_initial = [l0,compl1,compl2,max_ang1,max_ang2]
+#%%
+points0 = initialize_points(total_length, joint_length, l0)
+trajectory0 = simulate_trajectory(points0, compl1, compl2, max_ang1, max_ang2)
+cost0 = trajectory_cost(trajectory0,waypoints)
+
+#%%
+objective = partial(get_params_cost,total_length = 100, joint_length = 10,waypoints=waypoints)
+opt_bounds = [(2,98),(0.1,40),(0.1,40),(0,2*np.pi),(0,2*np.pi)]
+res = skopt.gp_minimize(objective,opt_bounds,x0 = x_initial)
+x_opt = res.x
+cost_opt = res.fun
+print("optimal x: {}\nfinal_cost: {}\n".format(x_opt,cost_opt))
+#%%
+
+points_opt = initialize_points(total_length, joint_length, x_opt[0])
+trajectory_opt = simulate_trajectory(points_opt, x_opt[1], x_opt[2], x_opt[3], x_opt[4])
+cost = trajectory_cost(trajectory_opt,waypoints)
+#%%
+for point in trajectory0:
+    plt.scatter(point[0],point[1],c='r', s = 4)
+for point in trajectory_opt:
+    plt.scatter(point[0],point[1],c='b', s = 4)
+plt.scatter(waypoints[:,0],waypoints[:,1],c='g',s=144, marker='*')
+
+plt.show()
+
+#%%
+if visualize:
+    fig= plt.figure(frameon=False)
+    viewport = fig.add_axes([0,0,1,1])
+    viewport.xaxis.set_visible(False)
+    viewport.yaxis.set_visible(False)
+    bound = total_length+10
+    viewport.axis('equal')
+    viewport.set_xlim(-bound,bound)
+    viewport.set_ylim(-bound,bound)
+    draw_lines0 = partial(draw_lines,viewport,points0)
+    artists = draw_lines0()
+    # %%
+    fps = 60
+    #writer = animation.writers['ffmpeg']
+    writer = animation.FFMpegFileWriter(fps = fps, metadata=dict(artist='Keene Chin'), bitrate=1000)
+
+    ani = animation.FuncAnimation(fig,func=update_frame, interval=1000//fps, frames=points_generator, blit=True, fargs=[viewport, artists])
+    ani.save('./finger_l{}_c{}_c{}.mp4'.format(l0, compl1, compl2), writer=writer)
 # %%
 
 
